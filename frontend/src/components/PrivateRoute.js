b3b795b85a5eba139d4568bb4e6074e1c9ddc0d0
Sri Gowtham Vishnu vamsi <nodagalasrigowtham1103@gmail.com>
2026-07-26 22:45:58 +0100

    Add PrivateRoute component for protected routes

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+function PrivateRoute({ children, roles }) {
+const { user } = useAuth();
+
+if (!user) {
+return <Navigate to="/login" replace />;
+}
+
+if (roles && roles.length > 0 && !roles.includes(user.role)) {
+return <Navigate to="/" replace />;
+}
+
+return children;
+}
+
+export default PrivateRoute;
